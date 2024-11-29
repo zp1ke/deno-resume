a9ba982eb323011eea47685409e24dba66448567
Michel Atucha <zp1ke@outlook.com>
2024-11-29 13:53:22 +0000

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: [
-    "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
+    "{components,routes,islands,routes,utils}/**/*.{ts,tsx,js,jsx}",
   ],
   theme: {
     fontFamily: {
